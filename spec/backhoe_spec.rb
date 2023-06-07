e42cc8bc0fb83a543aa4ff8fb9c5bfcabdc201e6
Micah Geisel <micah@botandrose.com>
2023-06-07 17:52:28 +0200

    testing the new implementation sounds rather impossible without testing its implementation. revisit later.

--- spec/backhoe_spec.rb
+++ spec/backhoe_spec.rb
@@ -23,16 +23,4 @@ RSpec.describe Backhoe do
       Backhoe.load "spec/support/example.sql", drop_and_create: true
     end
   end
-
-  describe ".backup" do
-    it "works" do
-      Timecop.freeze(Time.utc(2008, 9, 1, 10, 5, 0)) do
-        file_name = "2008-09-01T10:05:00Z.sql.gz"
-        file_path = "/tmp/#{file_name}"
-        expect(Backhoe).to receive(:dump).with(file_path)
-        expect(Kernel).to receive(:system).with("aws s3 mv #{file_path} s3://bucket/project/#{file_name}")
-        Backhoe.backup "bucket/project"
-      end
-    end
-  end
 end
